8c3d423bd0c1799d4f7cf41ac0aaa5b52d28f179
Gabriel Ullmann <ullmanng162@gmail.com>
2022-12-13 13:46:34 -0500

    fix eclipse project structure

--- src/com/assignment4/group24/Ticket.java
+++ src/com/assignment4/group24/Ticket.java
@@ -5,12 +5,12 @@ import java.util.UUID;
 public class Ticket {
 	UUID id;
 	
-	public Ticket(Booking booking) {	
+	public Ticket() {	
 		this.id = UUID.randomUUID();
 	}
 	
 	public String getId() {
 		// e.g. this could be in a QR code which is scanned at the station before you board
-		return this.id;
+		return this.id.toString();
 	}
 }
